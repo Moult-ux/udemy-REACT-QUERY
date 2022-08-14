0290d56c527880ab08278309880ad3d6c2685f9f
Moult <vincent.barbot@weenove.fr>
2022-08-15 00:27:10 +0200

    ready to custom hook staff exo

--- base-lazy-days/client/src/components/app/App.tsx
+++ base-lazy-days/client/src/components/app/App.tsx
@@ -1,13 +1,13 @@
-import { ChakraProvider } from '@chakra-ui/react';
-import { QueryClientProvider } from 'react-query';
-import { ReactQueryDevtools } from 'react-query/devtools'
-import { ReactElement } from 'react';
+import { ChakraProvider } from "@chakra-ui/react";
+import { QueryClientProvider } from "react-query";
+import { ReactQueryDevtools } from "react-query/devtools";
+import { ReactElement } from "react";
 
-import { queryClient } from '../../react-query/queryClient';
-import { theme } from '../../theme';
-import { Loading } from './Loading';
-import { Navbar } from './Navbar';
-import { Routes } from './Routes';
+import { queryClient } from "../../react-query/queryClient";
+import { theme } from "../../theme";
+import { Loading } from "./Loading";
+import { Navbar } from "./Navbar";
+import { Routes } from "./Routes";
 
 export function App(): ReactElement {
   return (
@@ -16,7 +16,7 @@ export function App(): ReactElement {
         <Navbar />
         <Loading />
         <Routes />
-        <ReactQueryDevtools initialIsOpen />
+        <ReactQueryDevtools />
       </QueryClientProvider>
     </ChakraProvider>
   );
--- base-lazy-days/client/src/components/treatments/hooks/useTreatments.ts
+++ base-lazy-days/client/src/components/treatments/hooks/useTreatments.ts
@@ -1,20 +1,20 @@
-import { useQuery } from 'react-query';
+import { useQuery } from "react-query";
 
-import type { Treatment } from '../../../../../shared/types';
-import { axiosInstance } from '../../../axiosInstance';
-import { queryKeys } from '../../../react-query/constants';
-import { useCustomToast } from '../../app/hooks/useCustomToast';
+import type { Treatment } from "../../../../../shared/types";
+import { axiosInstance } from "../../../axiosInstance";
+import { queryKeys } from "../../../react-query/constants";
 
 // for when we need a query function for useQuery
 async function getTreatments(): Promise<Treatment[]> {
-  const { data } = await axiosInstance.get('/treatments');
+  const { data } = await axiosInstance.get("/treatments");
+  console.log("getTreatments " + JSON.stringify(data));
   return data;
 }
 
 export function useTreatments(): Treatment[] {
-  // TODO: get data from server via useQuery
   const defaultData = [];
-  const { data = defaultData } = useQuery(queryKeys.TREATMENTS, getTreatments);
+  const { data = defaultData } = useQuery(queryKeys.treatments, getTreatments);
 
+  console.log("use treatments " + JSON.stringify(data));
   return data;
 }
